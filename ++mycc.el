a9486f92bb75ad41c0843ff81767d7199169c50d
2PoL <onpluto@outlook.com>
2021-05-17 08:10:41 +0800

    change to lsp mode

--- ++mycc.el
+++ ++mycc.el
@@ -14,13 +14,13 @@
   (setq c-default-style "my-cc"))
 
 ;; clangd with eglot
-(use-package! eglot
-  :defer 5
-  :config
-  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
-  (add-hook 'c++-mode-hook 'eglot-ensure)
-  (add-hook 'c-mode-hook 'eglot-ensure)
-  )
+;; (use-package! eglot
+;;   :defer 5
+;;   :config
+;;   (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+;;   (add-hook 'c++-mode-hook 'eglot-ensure)
+;;   (add-hook 'c-mode-hook 'eglot-ensure)
+;;   )
 
 ;; evil match it
 (use-package! evil-matchit
@@ -32,42 +32,20 @@
   )
 
 ;; clangd with lsp
-;; (setq lsp-clients-clangd-args '("-j=3"
-;;                                 "--background-index"
-;;                                 "--clang-tidy"
-;;                                 "--completion-style=detailed"
-;;                                 "--header-insertion=never"))
-;; (after! lsp-clangd (set-lsp-priority! 'clangd 2))
+(setq lsp-clients-clangd-args '("-j=3"
+                                "--background-index"
+                                "--clang-tidy"
+                                "--completion-style=detailed"
+                                "--header-insertion=never"))
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))
 
-;; (setq +format-with-lsp nil)
+(setq +format-with-lsp nil)
 
-;; (after! lsp-mode
-;;   (setq lsp-print-io nil
-;;         lsp-file-watch-threshold 3000)
-;;   (dolist (dir '("[/\\\\]\\.ccls-cache$"
-;;                  "[/\\\\]\\.mypy_cache$"
-;;                  "[/\\\\]\\.pytest_cache$"
-;;                  "[/\\\\]\\.cache$"
-;;                  "[/\\\\]\\.clwb$"
-;;                  "[/\\\\]_build$"
-;;                  "[/\\\\]__pycache__$"
-;;                  "[/\\\\]bazel-bin$"
-;;                  "[/\\\\]bazel-code$"
-;;                  "[/\\\\]bazel-genfiles$"
-;;                  "[/\\\\]bazel-out$"
-;;                  "[/\\\\]bazel-testlogs$"
-;;                  "[/\\\\]third_party$"
-;;                  "[/\\\\]third-party$"
-;;                  ))
-
-;;     (push dir lsp-file-watch-ignored))
-;;   )
-
-;; (after! lsp-ui
-;;   (setq lsp-ui-sideline-enable nil
-;;         lsp-ui-imenu-enable nil
-;;         lsp-ui-sideline-enable nil
-;;         lsp-ui-doc-include-signature nil
-;;         lsp-ui-doc-max-height 15
-;;         lsp-ui-doc-max-width 100
-;;         lsp-ui-doc-position 'at-point))
+(after! lsp-ui
+  (setq lsp-ui-sideline-enable nil
+        lsp-ui-imenu-enable nil
+        lsp-ui-sideline-enable nil
+        lsp-ui-doc-include-signature nil
+        lsp-ui-doc-max-height 15
+        lsp-ui-doc-max-width 20
+        lsp-ui-doc-position 'at-point))
--- init.el
+++ init.el
@@ -100,8 +100,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup             ; navigate your code and its documentation
-       (lsp
-        +eglot)
+       lsp
        (magit             ; a git porcelain for Emacs
         +forge)
        ;;make              ; run make tasks from Emacs
